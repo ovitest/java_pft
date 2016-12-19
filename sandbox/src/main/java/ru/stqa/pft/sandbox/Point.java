5b502438cca56457db5c1c3b4abefcd95aaad639
ovitest <vova4ko@gmail.com>
2016-12-19 22:32:39 +0200

    Добавлена функция вычисления расстояния между двумя точками

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Vladimir on 19.12.2016.
+ */
+public class Point {
+  public double x;
+  public double y;
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Vladimir on 19.12.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+
+  }
+}

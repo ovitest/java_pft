b80c7b86576ff1ac81028c6ecefb1d01a218694f
ovitest <vova4ko@gmail.com>
2016-12-23 01:24:00 +0200

    Добавлен тест для класса Square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -4,7 +4,7 @@ public class Distance {
 
 	public static void main(String[] args) {
 
-		Point p1 = new Point(2, 3);
+		Point p1 = new Point(3, 3);
 		Point p2 = new Point(5, 6);
 
 		System.out.println("Расстояние между точками с координатами " +"x="+p1.x + ",y="+ p1.y + " и " + "x="+p2.x + ",y="+p2.y+" = "+ p1.calcDistance(p2));

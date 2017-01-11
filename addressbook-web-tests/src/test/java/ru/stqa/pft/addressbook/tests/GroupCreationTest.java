c2a7a2d4945b2e59c64ab5385343b7a391d353f0
ovitest <vova4ko@gmail.com>
2017-01-11 17:37:40 +0200

    Заполнение формы оптимизировано, не меняется значение в поле елси значение совпадает со старым

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -10,7 +10,7 @@ public class GroupCreationTest extends TestBase{
 
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test", "test1", "test2"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returntoGroupPage();
     }

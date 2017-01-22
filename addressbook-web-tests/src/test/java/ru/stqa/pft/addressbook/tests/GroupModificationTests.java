a37f5906e84c7495beb1fc44fb07923c6827eb65
ovitest <vova4ko@gmail.com>
2017-01-22 23:51:47 +0200

    Добавлены проверки на количество групп для создания/удаления/аодификации группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,16 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-/**
- * Created by bcxtim on 03.01.2017.
- */
+
 public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
@@ -19,6 +19,8 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("test", "test1", "test2"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returntoGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before );
 
   }
 }

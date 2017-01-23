e72ab8af72890ed25882877fe9921ef1a44d5e73
ovitest <vova4ko@gmail.com>
2017-01-23 17:06:16 +0200

    Тест для создания группы модифицирован при помощи лямбда-выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,13 +20,7 @@ public class GroupCreationTest extends TestBase{
         Assert.assertEquals(after.size(), before.size() + 1);
 
 
-        int max = 0;
-        for (GroupData g: after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }

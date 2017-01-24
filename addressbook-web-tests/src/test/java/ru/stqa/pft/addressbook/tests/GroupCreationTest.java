add95550559926b4f8dbdff980da492cbae28309
ovitest <vova4ko@gmail.com>
2017-01-24 17:49:42 +0200

    Revert "Добавлена сортировка списков в тесты создания и модификации групп"
    This reverts commit c7bec234ec4f45af87882a052e55f1c27154223d.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,11 +20,9 @@ public class GroupCreationTest extends TestBase{
         Assert.assertEquals(after.size(), before.size() + 1);
 
 
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        Comparator<? super GroupData> byId = (g1, g2)  -> Integer.compare(g1.getId(), g2.getId());
-        before.sort(byId);
-        after.sort(byId);
-        Assert.assertEquals(before,after);
+        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
 
 }

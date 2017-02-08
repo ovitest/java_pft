ed28db869ddd3b12d5af223d301feb7a4401e865
ovitest <vova4ko@gmail.com>
2017-02-08 17:40:31 +0200

    Реализовано извлечение данных о контактах и угрппах из базы при помощи Hibernate

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,42 +3,89 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
-
+@Entity
 @XStreamAlias("contact")
+@Table (name = "addressbook")
 public class ContactData {
+  @Id
+  @Column(name = "id")
   @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "firstname")
   private String name;
+
+  @Column(name = "middlename")
   private String middlename;
+
   @Expose
+  @Column(name = "lastname")
   private String lastname;
+
+  @Column(name = "nickname")
   private String nickname;
+
+  @Column(name = "title")
   private String title;
+
+  @Column(name = "company")
   private String company;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "address")
   private String address;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "home")
   private String home;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "mobile")
   private String mobile;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "work")
   private String work;
+
+  @Transient
   private String allPhones;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "email")
   private String email;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "email2")
   private String email2;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "email3")
   private String email3;
+
+  @Transient
   private String allEmails;
+
+  @Transient
   private String group;
-  private File photo;
+
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
@@ -196,7 +243,7 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   @Override

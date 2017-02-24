7a7149aa824f9286da1f8e7be8a13a6cf9a39512
ovitest <vova4ko@gmail.com>
2017-02-24 16:32:39 +0200

    Добавлен тест геоайпи с использованием апи

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp(){
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("87.248.179.163");
+    assertEquals(geoIP.getCountryCode(), "MDA");
+  }
+}
